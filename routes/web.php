064384ed432eabcc78da0fbc19177789ee460918
tiwu <theimaginationwasunbroken@gmail.com>
2018-09-05 02:10:58 +0900

    logic no login

--- routes/web.php
+++ routes/web.php
@@ -11,38 +11,41 @@
 |
 */
 
-// トップ
-Route::get('/', 'TopController@index');
+Route::group(['middleware' => 'login'], function () {
 
-// 写真詳細
-Route::get('/photos/{id}', 'PhotoController@show')->where('id', '[0-9]+');
+    // トップ
+    Route::get('/', 'TopController@index');
 
-// 写真投稿
-Route::get('/photos/create', 'PhotoController@create');
-Route::post('/photos', 'PhotoController@store');
+    // 写真詳細
+    Route::get('/photos/{id}', 'PhotoController@show')->where('id', '[0-9]+');
 
-// 写真編集
-Route::get('/photos/{id}/edit', 'PhotoController@edit')->where('id', '[0-9]+');
-Route::put('/photos/{id}', 'PhotoController@update')->where('id', '[0-9]+');
+    // 写真投稿
+    Route::get('/photos/create', 'PhotoController@create');
+    Route::post('/photos', 'PhotoController@store');
 
-// 写真削除
-Route::delete('/photos/{id}', 'PhotoController@destory')->where('id', '[0-9]+');
+    // 写真編集
+    Route::get('/photos/{id}/edit', 'PhotoController@edit')->where('id', '[0-9]+');
+    Route::put('/photos/{id}', 'PhotoController@update')->where('id', '[0-9]+');
 
-// アニメ一覧
-Route::get('/anime', 'AnimeController@index');
+    // 写真削除
+    Route::delete('/photos/{id}', 'PhotoController@destory')->where('id', '[0-9]+');
 
-// アニメ詳細
-Route::get('/anime/{id}', 'AnimeController@show')->where('id', '[0-9]+');
+    // アニメ一覧
+    Route::get('/anime', 'AnimeController@index');
 
-// ユーザー詳細
-Route::get('/users/{id}', 'UserController@show')->where('id', '[0-9]+');
+    // アニメ詳細
+    Route::get('/anime/{id}', 'AnimeController@show')->where('id', '[0-9]+');
 
-// 問い合わせ
-Route::get('/inquiry', 'TopController@inquiry');
-Route::post('/inquiry', 'TopController@inquiryStore');
+    // ユーザー詳細
+    Route::get('/users/{id}', 'UserController@show')->where('id', '[0-9]+');
 
-// ガイドライン
-Route::get('/guide', 'TopController@guide');
+    // 問い合わせ
+    Route::get('/inquiry', 'TopController@inquiry');
+    Route::post('/inquiry', 'TopController@inquiryStore');
 
-// サイトについて
-Route::get('/about', 'TopController@about');
+    // ガイドライン
+    Route::get('/guide', 'TopController@guide');
+
+    // サイトについて
+    Route::get('/about', 'TopController@about');
+});
