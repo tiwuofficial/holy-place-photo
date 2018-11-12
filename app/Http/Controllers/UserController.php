388dd2e57adf71a84d01dcce421916df8b045560
tiwu <theimaginationwasunbroken@gmail.com>
2018-11-12 20:53:04 +0900

    create user detail

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Model\Photo;
+use App\User;
+
 class UserController extends Controller
 {
 
@@ -10,8 +13,11 @@ class UserController extends Controller
      *
      * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
      */
-    public function show()
+    public function show(User $user)
     {
-        return view('user.show');
+        $userPhotos = Photo::where('user_id', $user->id)->get();
+        $title = $user->name . 'さんの聖地の写真一覧。【Holy Place Photo】アニメの聖地の写真の投稿';
+        $description =  $user->name . 'さんの聖地の写真一覧です。【Holy Place Photo】はアニメの聖地の写真の共有サイトです。';
+        return view('user.show', compact('title', 'description', 'userPhotos', 'user'));
     }
 }
---
+++ resources/assets/js/entry/user/show.js
@@ -0,0 +1,5 @@
+import '../../common/base';
+
+new Vue({
+  el: '#wrapper',
+});
